5fd474168a039c2e328607a86e5ed3bec515d542
Gerard Matthew <gerrymf55@gmail.com>
2026-01-17 13:38:58 +0400

    adjuted my audio

--- music-player.js
+++ music-player.js
@@ -16,7 +16,7 @@
   let globalCurrentIndex = 0;
   let globalIsPlaying = false;
   let globalIsMuted = false;
-  const DEFAULT_VOLUME = 60; 
+  const DEFAULT_VOLUME = 40; 
   let youtubeAPIReady = false;
   let isAdvancing = false; 
   let playbackPositionInterval = null; 
@@ -194,7 +194,7 @@
       
       globalIsMuted = shouldBeMuted;
       setIsMuted(shouldBeMuted);
-      globalAudio.volume = shouldBeMuted ? 0 : DEFAULT_VOLUME / 50;
+      globalAudio.volume = shouldBeMuted ? 0 : DEFAULT_VOLUME / 100;
       
       // Load and play audio
       globalAudio.src = audioPath;

95b56f13806f8d7fc316edd700d3af7a5ad6228d
Gerard Matthew <144931668+N1X-KUN@users.noreply.github.com>
2026-01-17 14:01:13 +0400

    Change default volume from 40 to 15

--- music-player.js
+++ music-player.js
@@ -16,7 +16,7 @@
   let globalCurrentIndex = 0;
   let globalIsPlaying = false;
   let globalIsMuted = false;
-  const DEFAULT_VOLUME = 40; 
+  const DEFAULT_VOLUME = 15; 
   let youtubeAPIReady = false;
   let isAdvancing = false; 
   let playbackPositionInterval = null; 
